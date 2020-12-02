bf484b7769f04a959270e2152da8599e54328005
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-12-02 11:48:08 -0300

    Atributos adicionados

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CadastroAlunoPOO.classes;
+
+namespace CadastroAlunoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ classes/Aluno.cs
@@ -0,0 +1,13 @@
+namespace CadastroAlunoPOO.classes
+{
+    public class Aluno
+    {
+        public string nome;
+        public string curso;
+        public string rg;
+        public int idade;
+        public bool bolsista;
+        public float mediaFinal;
+        public float valorMensalidade;
+    }
+}
